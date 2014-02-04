5857f5281a5a7c3c7d03273f1eb5105b2cb4ecd5
fredyfire <pmextra@gmail.com>
2014-02-04 14:16:58 +0100

    Update openMeteoData.js
    typo

--- openMeteoData.js
+++ openMeteoData.js
@@ -1,10 +1,10 @@
 {
 	"documentDescription":
 	{
-	"type":"openMeteoData (v. 0.1.1)",
-	"licence":"openMeteoData (v. 0.1.1)",
-	"buildDate":"2014-02-01 00:00:01",
-	"generator":"openMeteoData Builder (v .0.1.1)"
+		"type":"openMeteoData (v. 0.1.1)",
+		"licence":"openMeteoData (v. 0.1.1)",
+		"buildDate":"2014-02-01 00:00:01",
+		"generator":"openMeteoData Builder (v .0.1.1)"
 	},
 
 	
